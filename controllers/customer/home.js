ac7b0a758c866954175638b72c98e9688e253b20
MdEstiakAhmed <estiak97@gmail.com>
2020-02-27 15:03:06 +0600

    product show by category

--- controllers/customer/home.js
+++ controllers/customer/home.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var userModel = require.main.require('./models/user-model');
+var productModel = require.main.require('./models/product-model');
 var router = express.Router();
 
 
@@ -8,6 +9,30 @@ router.get('/home', function(request, response){
 		userModel.getByEmail(request.cookies['loginUser'], function(result){
             console.log("customer home controllers");
 			response.render('customer/home', {user: result});
+		});
+	}else{
+		response.redirect('/');
+	}
+});
+
+router.get('/showProduct/:category', function(request, response){
+	if(request.cookies['loginUser'] != null){
+		productModel.getByCategory(request.params.category, function(result){
+			console.log("monitor click");
+			console.log("product: ", result);
+			response.render('customer/showProduct', {cat: result});
+		});
+	}else{
+		response.redirect('/');
+	}
+});
+
+router.get('/showProduct/:subCategory', function(request, response){
+	if(request.cookies['loginUser'] != null){
+		productModel.getByCategory(request.params.category, function(result){
+			console.log("monitor click");
+			console.log("product: ", result);
+			response.render('customer/showProduct', {cat: result});
 		});
 	}else{
 		response.redirect('/');
---
+++ models/product-model.js
@@ -0,0 +1,44 @@
+var db = require('./db');
+
+module.exports = {
+    getByProduct : function(id, callback){
+		var sql = "select * from product where p_category_id = (select p_category_id from product_category where p_category_name = ?)";
+		db.getResults(sql, function(results){
+			if(results.length > 0){
+				callback(results[0]);
+			}else{
+				callback(null);
+			}
+		});
+	},
+	getByCompany : function(id, callback){
+		var sql = "select * from product where id="+id;
+		db.getResults(sql, function(results){
+			if(results.length > 0){
+				callback(results[0]);
+			}else{
+				callback(null);
+			}
+		});
+	},
+	getBySubCategory : function(subCategory, callback){
+		var sql = "select * from product where p_sub_category_id = (select p_sub_category_id from product_sub_category where p_category_id = (select p_category_id from product_category where p_category_name = ?))";
+		db.getResults(sql, [subCategory], function(results){
+			if(results.length > 0){
+				callback(results);
+			}else{
+				callback(null);
+			}
+		});
+	},
+	getByCategory : function(category, callback){
+		var sql = "select * from product where p_category_id = (select p_category_id from product_category where p_category_name = ?)";
+		db.getResults(sql, [category], function(results){
+			if(results.length > 0){
+				callback(results);
+			}else{
+				callback(null);
+			}
+		});
+	}
+}
